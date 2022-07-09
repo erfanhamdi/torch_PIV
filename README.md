8e2142a9320d350dd71843e40f69e674dbaff36a
erfanhamD <erfan.hamdi@gmail.com>
2022-07-09 11:06:30 +0430

    refactoring the code

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ validation of the results of this algorithm was done with the openPIV software.
 
 The table below shows the results of this algorithm on a free jet injection experiment.
 
-|        R Correlation   | Streamline | 
-|:----------:|:-------------:| 
-|![](figs/r_corr_jet_31_81.png)|![](figs/streamline_jet_31_81.png)
+|        Streamline   | Velocity Field | Correlation Field | 
+|:----------:|:-------------:|:-------------:|
+|![](figs/streamline.png)|![](figs/velocity_field.png)|![](figs/correlation_field.png)
 
 The results of the Validation using OpenPIV software with the same Interrogation Window size and same pair of images:
 ![](figs/openPIV_31_81.png)
--- main-ncc.py
+++ main-ncc.py
@@ -1,50 +1,49 @@
 # -*- coding: utf-8 -*-
 """
-Particle Image Velocimetry Using 
-pyTorch implemented Normalized Cross Correlation
-@author: Erfan Hamdi Jan 2022
+Particle Image Velocimetry Using pyTorch implemented Normalized Cross Correlation
+by: @erfanhamdi Jan 2022
 """
+import yaml
 
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-from tqdm import tqdm 
+
 import torch
 from torchvision import transforms
+
 import utils
-import warnings
-warnings.filterwarnings("ignore")
+
+# load the config from yaml file
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
 # Image transformations using pytorch
 image_transforms = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),
     transforms.RandomVerticalFlip(p=1),
+    # transforms.Pad(padding=81, padding_mode='edge'),
     transforms.ToTensor(),
 ])
 
 # Input images
-img_1 = Image.open('/Users/venus/Erfan/sharifCourses/optic/piv/PIV/Example/1.jpg')
+img_1 = Image.open(config["img_1_address"])
 img_1 = image_transforms(img_1)
 
-img_2 = Image.open('/Users/venus/Erfan/sharifCourses/optic/piv/PIV/Example/2.jpg')
+img_2 = Image.open(config["img_2_address"])
 img_2 = image_transforms(img_2)
 
-# Output Params
-frame = 2
-
 # Algorithm Params
-# Maximum Fixing Iterations
-i_fix = 500
-# R correlation threshold
-r_limit = 0.5
 # Spatial Scale [m/pixel]
-l_scale = 1.0
+l_scale = config["l_scale"]
 # Temporal Scale 1/frame_rate [s/frame]
-t_scale = 1.0
+t_scale = config["t_scale"]
 # Interrodation Windows Sizes (pixel)
-iw_size = 51 
+iw_size = config["iw_size"]
 # Search Windows Sizes (sw > iw) (pixel)
-sw_size = 81 
+sw_size = config["sw_size"]
+# Interrogation window overlap percentage
+overlap = config["overlap"]
 
 batch, height, width = img_1.shape
 
@@ -57,13 +56,9 @@ margin = int((sw_size - iw_size) / 2)
 iw_no_y = int(2*np.floor((height - 1 - iw_size) / (iw_size - 1)))
 iw_no_x = int(2*np.floor((width - 1 - iw_size) / (iw_size - 1)))
 
-overlap = 0.1
 iw_no_x = int(width/(int(iw_size - iw_size * overlap)+1))+1
 # Initializing Displacement field
 # Displacement in x and y direction
-vecx = np.zeros((iw_no_y, iw_no_x)) # x-Displacement
-vecy = np.zeros((iw_no_y, iw_no_x)) # y-Displacement
-vec = np.zeros((iw_no_y, iw_no_x)) # Magnitude
 vec_x_ = []
 vec_x_total = []
 vec_y_ = []
@@ -71,67 +66,65 @@ vec_y_total = []
 vec_ = []
 vec_total = []
 # Initializing the Correlation Matrix
-rij = np.zeros((iw_no_y, iw_no_x)) # Correlation coeff.
-rij2 = []
+rij = []
 rj = []
+
 iw_right_bound = 0
 iw_top_bound = 0
 iw_index = 0
 while iw_right_bound <= width:
-# for iw_x in tqdm(range(iw_no_x)):
+    # Moving in the X direction
+    # Calculation of the boundaries of the interrogation window
     iw_left_bound = int(iw_index * ((iw_size - 1) * (1-overlap)))
     iw_right_bound = iw_left_bound + iw_size
-
+    # Calculation of the boundaries of the search window
     sw_left_bound = max(0, iw_left_bound - margin)
     sw_d_diff = max(0, iw_left_bound - margin) - (iw_left_bound - margin)
     sw_right_bound = min(width - 1, iw_right_bound + margin)
-
-    # for iw_y in range(iw_no_y):
+    
     iw_y_index = 0
     iw_top_bound = 0
+    
     while iw_top_bound <= height:
+        # Moving in the Y direction
+        # Calculation of the boundaries of the interrogation window
         iw_bottom_bound = int(iw_y_index * ((iw_size - 1) * (1-overlap)))
         iw_top_bound = iw_bottom_bound + iw_size
-        
+        # Calculation of the boundaries of the search window
         sw_bottom_bound = max(0, iw_bottom_bound - margin)
         sw_l_diff = max(0, iw_bottom_bound - margin) - (iw_bottom_bound - margin)
         sw_top_bound = min(height - 1, iw_top_bound + margin)
-        
-        # R = np.zeros((sw_size - iw_size + 1, sw_size - iw_size + 1)) - 1 # Correlation Matrix
-        c1 = img_1[..., iw_bottom_bound:iw_bottom_bound + iw_size, iw_left_bound:iw_left_bound + iw_size]# IW from 1st image
+        # Getting an IW from the first image
+        c1 = img_1[..., iw_bottom_bound:iw_bottom_bound + iw_size, iw_left_bound:iw_left_bound + iw_size]
         center_pixel_c1_in_image_1 = torch.Tensor([iw_bottom_bound + iw_size // 2, iw_left_bound + iw_size // 2])
-        R_torch = utils.torch_corr(c1, img_2[..., sw_bottom_bound:sw_top_bound, sw_left_bound:sw_right_bound])
+        # Getting the SW from the second image
+        sw2 = img_2[..., sw_bottom_bound:sw_top_bound, sw_left_bound:sw_right_bound]
+        # Calculating the Correlation 
+        R_torch = utils.torch_corr(c1, sw2)
+        # Getting the Maximum Correlation
         R_max = R_torch.max()
         rj.append(R_max)
-        # rij[iw_y, iw_x] = R_torch.max()
-        # if rij[iw_y, iw_x] >= r_limit:
-        if R_max >= r_limit: 
-            # dum=np.floor(np.argmax(R)/R.shape[0])
+        # Calculating the displacement of the maximum correlation
+        if R_max >= config["r_limit"]:
+            # dummy variable to get the coordinate of the maximum correlation
             dum = np.unravel_index(np.argmax(R_torch), R_torch.shape)
+            # Finding out where the max corr happens in the second image
             center_of_max_R_in_image_2 = np.array([sw_bottom_bound + dum[2], sw_left_bound + dum[1]])
-            # vecy[iw_y, iw_x] = center_of_max_R_in_image_2[1] - center_pixel_c1_in_image_1[1] + utils.subpix(R_torch[0], 'y', dum)
+            # Calculating the displacement of the center of the IW from the first image and the place of max_corr 
+            # from the SW of the second image.
             y_displacement = center_of_max_R_in_image_2[1] - center_pixel_c1_in_image_1[1] + utils.subpix(R_torch[0], 'y', dum)
             vec_y_.append(y_displacement)
-            # vecx[iw_y, iw_x] = center_of_max_R_in_image_2[0] - center_pixel_c1_in_image_1[0] + utils.subpix(R_torch[0], 'x', dum)
             x_displacement = center_of_max_R_in_image_2[0] - center_pixel_c1_in_image_1[0] + utils.subpix(R_torch[0], 'x', dum)
             vec_x_.append(x_displacement)
-            # vecy[i,j]=dum-(margin-sw_l_diff)+subpix(R,'y')
-
-            # vecx[i,j]=np.argmax(R)-dum*R.shape[0]-(margin-sw_left_bound_diff)+subpix(R,'x')
             total_displacement = (x_displacement**2 + y_displacement**2)**0.5
-            # vec[iw_y, iw_x] = np.sqrt(vecx[iw_y, iw_x] * vecx[iw_y, iw_x] + vecy[iw_y, iw_x] * vecy[iw_y, iw_x])
             vec_.append(total_displacement)
-
         else:
-            # vecx[iw_y, iw_x]=0.0
             vec_x_.append(0.0)
             vec_y_.append(0.0)
             vec_.append(0.0)
-            # vecy[iw_y, iw_x]=0.0
-            # vec[iw_y, iw_x]=0.0
         iw_y_index += 1
     iw_index += 1
-    rij2.append(rj)
+    rij.append(rj)
     rj = []
     vec_x_total.append(vec_x_)
     vec_x_ = []
@@ -139,38 +132,50 @@ while iw_right_bound <= width:
     vec_y_ = []
     vec_total.append(vec_)
     vec_ = []
-    print(f"passed {iw_index}")
-rij = np.array(rij2).T
+    print(f"IW No. {iw_index}")
+
+rij = np.array(rij).T
 vec = np.array(vec_total).T
 vec_x = np.array(vec_x_total).T
 vec_y = np.array(vec_y_total).T
-vecx, vecy, vec, i_disorder, i_cor_done = utils.fixer(vec_x, vec_y, vec, rij, r_limit, i_fix)
 
+# Filtering out the outliers with the median
+vecx, vecy, vec, i_disorder, i_cor_done = utils.fixer(vec_x, vec_y, vec, rij, config["r_limit"], config["i_fix"])
+
+# Plotting Parameters
+output_dir = config["output_dir"]
 X, Y = np.meshgrid(np.arange(0.5*iw_size, 0.5*iw_size*(rij.shape[1]+1), 0.5*iw_size), 
                    np.arange(0.5*iw_size, 0.5*iw_size*(rij.shape[0]+1), 0.5*iw_size))
+
+# Spatial and Temporal scaling
 X *= l_scale
 Y *= l_scale
-
 vecx *= (l_scale / t_scale)
 vecy *= (l_scale / t_scale)
 vec *= (l_scale / t_scale)
 
-np.savez('results.npz', vecx = vecx, vecy = vecy, vec = vec, rij = rij)
+# Skipping boundary Interrogation Windows
+sk = 1 
+X = X[sk:-sk, sk:-sk]
+Y = Y[sk:-sk, sk:-sk]
+vecx = vecx[sk:-sk, sk:-sk]
+vecy = vecy[sk:-sk, sk:-sk]
+vec = vec[sk:-sk, sk:-sk]
+rij = rij[sk:-sk, sk:-sk]
 
-# res=np.load('results.npz'); vecx=res['vecx']; vecy=res['vecy']; vec=res['vec']; rij=res['rij']; # Load saved data
-# fig, ax = plt.subplots(figsize=(8,8*ia/ja))
-# q = ax.quiver(X, Y, vecx, vecy,units='width')
-# plt.savefig('velocity_jet.png', dpi=300)
-# plt.show()
+np.savez(f"{output_dir}/results.npz", vecx = vecx, vecy = vecy, vec = vec, rij = rij)
 
-fig, ax = plt.subplots(figsize = (8, 8 * height / width))
-plt.contourf(X[0], np.transpose(Y)[0], rij, cmap='jet', levels=np.arange(rij.min(), min(rij.max() + 0.1, 1.0), 0.01))
+fig, ax = plt.subplots(figsize=(10,8))
+q = ax.quiver(X, Y, vecx, vecy, units='width')
+plt.savefig(f"{output_dir}/figs/velocity_field.png", dpi=300)
+
+fig, ax = plt.subplots(figsize=(10,8))
+plt.contourf(X[0],np.transpose(Y)[0],rij,cmap='jet',levels=np.arange(rij.min(),min(rij.max()+0.1,1.0),0.01))
 plt.colorbar(label='R')
-# plt.savefig(f'/Users/venus/Erfan/sharifCourses/optic/piv/Report/figs/mixing/r_corr_jet_t{frame}_{iw}_{sw}.png', dpi=300)
-plt.show()
+plt.savefig(f"{output_dir}/figs/correlation_field.png", dpi=300)
 
-fig, ax = plt.subplots(figsize=(8, 8 * height / width))
+fig, ax = plt.subplots(figsize=(10,8))
 plt.streamplot(X, Y, vecx, vecy, density = 3, linewidth = 0.8, color = vec)
-# plt.savefig(f'/Users/venus/Erfan/sharifCourses/optic/piv/Report/figs/mixing/velocity_jet_t{frame}_{iw}_{sw}.png', dpi=300)
+plt.savefig(f"{output_dir}/figs/streamline.png", dpi=300)
 plt.colorbar(label='velocity')
 plt.show()
